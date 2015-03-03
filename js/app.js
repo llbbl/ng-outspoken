9166e8b7612216c7700971f0b406be18d295d0c8
kwoodfield <kcwoodfield@gmail.com>
2015-03-03 14:23:52 -0600

    Adding in the rating system

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 //  service or factory in the module
 //
 //
-var app = angular.module("outApp", ["firebase"]);
+var app = angular.module("outApp", ["firebase", 'ui.bootstrap']);
 
 
 
@@ -33,7 +33,36 @@ app.controller("outController", ["$scope", "$firebase",
 
         console.log($scope.data);
 
+
     }
 
 
 ]);
+
+
+app.controller('ratingController', function ($scope) {
+
+        $scope.rate = 0;
+        $scope.max = 5;
+        $scope.isReadonly = false;
+
+
+        $scope.hoveringOver = function(value) {
+            $scope.overStar = value;
+            $scope.percent = 100 * (value / $scope.max);
+        };
+
+        $scope.voteTally = function (value) {
+            alert(value);
+        }
+
+    $scope.ratingStates = [
+        {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+        {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+        {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+        {stateOn: 'glyphicon-heart'},
+        {stateOff: 'glyphicon-off'}
+    ];
+
+
+});
